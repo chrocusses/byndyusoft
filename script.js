884f4d44c8b47917b5f388389df0fba83637a0bf
Arthur <chrocusses@mail.ru>
2022-06-06 22:42:43 +0300

    add package.json

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ import f1 from './sumMins.mjs'
 import f2 from './getSumMins.mjs'
 import f3 from './minSum.mjs'
 
-document.querySelector('.method1').addEventListener('click', calculate1)
-document.querySelector('.method2').addEventListener('click', calculate2)
-document.querySelector('.method3').addEventListener('click', calculate3)
+document.querySelector('.method1').addEventListener('click', calc1)
+document.querySelector('.method2').addEventListener('click', calc2)
+document.querySelector('.method3').addEventListener('click', calc3)
 
-function calculate1() {
+function calc1() {
     const data = document.querySelector('.i-1').value
     const dataArray = data.split(",")
     document.querySelector('.result').innerHTML = 'Result: ' + f1(dataArray)
 }
 
-function calculate2() {
+function calc2() {
     const data = document.querySelector('.i-1').value
     const dataArray = data.split(",")
     document.querySelector('.result').innerHTML = 'Result: ' + f2(dataArray)
 }
 
-function calculate3() {
+function calc3() {
     const data = document.querySelector('.i-1').value
     const dataArray = data.split(",")
     document.querySelector('.result').innerHTML = 'Result: ' + f3(dataArray)
